bf56ea280dc9b79fb61468ceb414f84847892886
Alberto Eusebi <alberto@albeus.eu>
2020-04-16 19:24:33 +0100

    Add label to image, avoid login while creating keys.

--- golden-image/create_golden_image.sh
+++ golden-image/create_golden_image.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-# Create a base instance as a golden image for the CWL training
-# laboratory. The image will be then configured adding the applications
-# and the enviroment required for the tutorials.
+# First step to create a base instance as a golden image for the CWL
+# training laboratory. The image will be then configured adding the
+# applications and the enviroment required for the tutorials.
 
 
 gcloud compute instances create cwl-lab-golden \
@@ -17,10 +17,11 @@ gcloud compute instances create cwl-lab-golden \
     --boot-disk-type=pd-standard \
     --boot-disk-device-name=cwl-lab-golden \
     --tags=allow-ssh \
+    --labels=vmrole=golden-image \
     --metadata-from-file=user-data=./cloud-init.cfg
 
 echo -e "
 Connecting to the machine as \"ubuntu\" user. This way the user will
-be created and it will be available for further uses."
-
-gcloud compute ssh ubuntu@cwl-lab-golden --zone=europe-west2-b
+be created and it will be available for further uses.
+"
+gcloud compute ssh ubuntu@cwl-lab-golden --zone=europe-west2-b --command="lsb_release -a"
